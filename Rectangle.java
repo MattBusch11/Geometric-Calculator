94d3269b5a9b55f1dd72b68d322c87a07cc9782c
Matt Busch <mbb23738@email.vccs.edu>
2017-10-05 17:25:01 -0400

    Added input dialog and variable for input

--- Rectangle.java
+++ Rectangle.java
@@ -3,6 +3,7 @@ public class Rectangle {
 	public void start(Stage primaryStage) {
 		TextInputDialog textInputDialog = new TextInputDialog("Geometric Caluculator");
 		Optional<String> reult = textInputDialog.showAndWait();
-		String input = result.get();
+		String WidthInput = result.get();
+		double width = Double.parseDouble(input);
 	}
 }
